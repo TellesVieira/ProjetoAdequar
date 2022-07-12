cd63875ca9f7db911d267bb5098a221e8830eee7
unknown <wagnertelles@yahoo.com.br>
2022-07-12 18:29:07 -0300

    ajuste

--- public/index.php
+++ public/index.php
@@ -1,46 +1,55 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
 |
 */
 
-//Route::get('/', function () {
-   // return view('welcome');
-//});
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-Route::get('/', function () {
-   return view('home');
-});
-Route::get('/contato', function () {
-   return view('contato');
-});
-Route::get('/serviço-gerenciamento-mídias', function () {
-   return view('conteudo');
-});
-Route::get('/serviço-anúncio-trafego-pago', function () {
-   return view('gerenciamento');
-});
-Route::get('/serviço-identidade-visual', function () {
-   return view('identidade-visual');
-});
-Route::get('/serviço-material-offline', function () {
-   return view('material');
-});
-Route::get('/portfolio', function () {
-   return view('portfolio');
-});
-Route::get('/serviço-website', function () {
-   return view('website');
-});
-Route::get('/planejamento-estratégico-Marketing', function () {
-   return view('planejamento');
-});
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../ProjetoAdequar/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../ProjetoAdequar/bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
